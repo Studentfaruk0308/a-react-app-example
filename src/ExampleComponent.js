d789bb3eb30d1dc4832e1c1dc6dce3c9db2d7512
faruk <studentfaruk0308@gmail.com>
2022-05-06 10:27:36 +1000

    On 06052022

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Faruk from './Faruk';
 
 // no need to modify anything in here. The second test is looking for the returned JSX from this component
 class ExampleComponent extends Component {
@@ -7,7 +8,13 @@ class ExampleComponent extends Component {
       <div className="example">
         <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
         <p>Whoa!</p>
+        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
+        <p>Whoa!</p>
+        <br />
+        <Faruk />
+        <br />
       </div>
+      
     );
   }
 }
---
+++ src/Faruk.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// no need to modify anything in here. The second test is looking for the returned JSX from this component
+class Faruk extends Component {
+  render() {
+    return (
+      <div className="Faruk">
+        Hi This is Md Faruk Ahmed
+      </div>
+      
+    );
+  }
+}
+
+export default Faruk;
